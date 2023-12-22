15ccd658e19af5fa546f807f2ba36e1496d76232
Mkr1984 <marcelkroeze@gmail.com>
2023-12-22 08:22:13 +0800

    Add categories and descriptions to DienstenPage

--- theblackstag-app/src/app/diensten/page.tsx
+++ theblackstag-app/src/app/diensten/page.tsx
@@ -1,6 +1,36 @@
 import Intro from "@/components/Home/Intro";
+import Link from "next/link";
 
 const DienstenPage: React.FC = () => {
+  const content = {
+    categories: [
+      {
+        title: "Visuele identiteit",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed blandit mauris. Phasellus eget pellentesque est. Sed vitae sapien quam. Ut ut neque hendrerit, sollicitudin nisi sed, tincidunt mauris. Donec orci odio, tempus vel fermentum eget, blandit non lacus. Nullam commodo quam nec tempus ornare. Proin ex elit, facilisis et orci eget, euismod sodales lectus. In non tellus est. Aliquam et tellus magna.",
+      },
+      {
+        title: "Kaarten en uitnodigingen",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed blandit mauris. Phasellus eget pellentesque est. Sed vitae sapien quam. Ut ut neque hendrerit, sollicitudin nisi sed, tincidunt mauris. Donec orci odio, tempus vel fermentum eget, blandit non lacus. Nullam commodo quam nec tempus ornare. Proin ex elit, facilisis et orci eget, euismod sodales lectus. In non tellus est. Aliquam et tellus magna.",
+      },
+      {
+        title: "Promotie/ uitingen",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed blandit mauris. Phasellus eget pellentesque est. Sed vitae sapien quam. Ut ut neque hendrerit, sollicitudin nisi sed, tincidunt mauris. Donec orci odio, tempus vel fermentum eget, blandit non lacus. Nullam commodo quam nec tempus ornare. Proin ex elit, facilisis et orci eget, euismod sodales lectus. In non tellus est. Aliquam et tellus magna.",
+      },
+      {
+        title: "Foto/ videografie",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed blandit mauris. Phasellus eget pellentesque est. Sed vitae sapien quam. Ut ut neque hendrerit, sollicitudin nisi sed, tincidunt mauris. Donec orci odio, tempus vel fermentum eget, blandit non lacus. Nullam commodo quam nec tempus ornare. Proin ex elit, facilisis et orci eget, euismod sodales lectus. In non tellus est. Aliquam et tellus magna.",
+      },
+      {
+        title: "Hulp op de werkvloer",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed blandit mauris. Phasellus eget pellentesque est. Sed vitae sapien quam. Ut ut neque hendrerit, sollicitudin nisi sed, tincidunt mauris. Donec orci odio, tempus vel fermentum eget, blandit non lacus. Nullam commodo quam nec tempus ornare. Proin ex elit, facilisis et orci eget, euismod sodales lectus. In non tellus est. Aliquam et tellus magna.",
+      },
+    ],
+  };
   return (
     <main>
       <Intro />
@@ -28,8 +58,28 @@ const DienstenPage: React.FC = () => {
             feugiat mollis ex purus at ligula. Nulla justo lacus, vulputate sit
             amet libero quis, gravida aliquam sem. In dignissim quam ipsum, at
             sollicitudin neque molestie pulvinar. Donec sodales nulla vel
-            condimentum efficitur.{" "}
+            condimentum efficitur.
           </p>
+        </div>
+      </section>
+      <section className="container mt-16">
+        <div>
+          <h2 className="font-medium text-xl">Waarmeen kan ik je helpen?</h2>
+          {content.categories.map((category) => (
+            <div className="md:flex items-start justify-start border-t min-h-40 py-16">
+              <div className="md:w-1/3">
+                <div className="max-w-xs">
+                  <Link
+                    href={"/diensten/"}
+                    className="w-fit max-w-xs font-medium text-3xl border-b leading-relaxed hover:bg-gradient-to-t hover:from-50% hover:to-50% hover:from-tbsgreen hover:border-b-0"
+                  >
+                    {category.title}
+                  </Link>
+                </div>
+              </div>
+              <p className="md:w-2/3 ">{category.description}</p>
+            </div>
+          ))}
         </div>
       </section>
     </main>
