94a799adbb3e62b6a3a3c0d09b8ba40ebf80ae68
Mkr1984 <marcelkroeze@gmail.com>
2023-12-28 17:05:48 +0800

    Add image component and update team member details

--- theblackstag-app/src/components/Team/index.tsx
+++ theblackstag-app/src/components/Team/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Team = () => {
@@ -5,47 +6,64 @@ const Team = () => {
     {
       name: "Katja van de Ven",
       role: "creative eigenaar",
-      image: "/images/member1.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team1.jpg",
     },
     {
       name: "Katja van de Ven",
       role: "designer",
-      image: "/images/member1.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team2.jpg",
     },
     {
       name: "Katja van de Ven",
       role: "Foto/ videograaf",
-      image: "/images/member1.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team3.jpg",
     },
     {
       name: "Katja van de Ven",
       role: "media adviseur",
-      image: "/images/member1.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team1.jpg",
     },
     {
       name: "Katja van de Ven",
       role: "administratie",
-      image: "/images/member1.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team2.jpg",
     },
     {
       name: "Luna",
       role: "Bedrijfsvogel",
-      image: "/images/member2.jpg",
+      hashtags: "#canon #fotovandedag #uitdehandgelopenhobby",
+      image: "/images/team3.jpg",
     },
   ];
 
   return (
     <section className="container text-white py-20">
-      <div className="flex flex-wrap">
+      <h3 className="text-4xl text-center font-bold mb-12">Het team</h3>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
         {teamMembers.map((member, index) => (
-          <div
-            key={index}
-            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2"
-          >
-            <img src={member.image} alt={member.name} className="w-full" />
-            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2">
-              <h3 className="text-lg font-bold">{member.name}</h3>
-              <p>Role: {member.role}</p>
+          <div key={index} className="w-full p-2">
+            <div className="relative w-fit bg-tbsgreen mx-auto group">
+              <Image
+                src={member.image}
+                width={360}
+                height={540}
+                alt={member.name}
+                className="group-hover:opacity-10 transition-opacity duration-500 ease-in-out"
+              />
+              <div className="absolute bottom-0 left-0 w-full h-1/6">
+                <p className="hidden px-12 text-xl group-hover:block">
+                  {member.hashtags}
+                </p>
+              </div>
+            </div>
+            <div className="text-center py-4">
+              <h3 className="text-3xl font-bold text-white">{member.name}</h3>
+              <p className="text-xl text-gray-300">{member.role}</p>
             </div>
           </div>
         ))}
