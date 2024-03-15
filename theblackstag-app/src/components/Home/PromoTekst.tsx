6fee630aae6cd9c9c769bada30e208a983f38eb2
Mkr1984 <marcelkroeze@gmail.com>
2024-03-15 14:34:29 +0800

    Refactor PromoTekst component to use Next.js Link component

--- theblackstag-app/src/components/Home/PromoTekst.tsx
+++ theblackstag-app/src/components/Home/PromoTekst.tsx
@@ -1,12 +1,29 @@
+import Link from "next/link";
+
 export default function PromoTekst() {
-  const promoTeksthtmlContent =
-    "<p>The Black stag creëert <a href='#'>communicatiemiddelen</a> om jouw bedrijf beter op de kaart te zetten. Van <a href='#'>huisstijl ontwikkeling</a> tot de volledige <a href='#'>signing</a> van je pand. Naast de B2B en B2C is The Black Stag ook actief voor particulieren. Zo kun je hier terecht voor unieke <a href='#'>geboorte-/ en trouwkaarten</a>.</p>";
+  const promoTeksthtmlContent = (
+    <>
+      <p>
+        The Black stag creëert{" "}
+        <Link href="/diensten">communicatiemiddelen</Link> om jouw bedrijf beter
+        op de kaart te zetten. Van{" "}
+        <Link href="/diensten/visuele-identiteit">huisstijl ontwikkeling</Link>{" "}
+        tot de volledige{" "}
+        <Link href="/diensten/visuele-identiteit">signing</Link> van je pand.
+        Naast de B2B en B2C is The Black Stag ook actief voor particulieren. Zo
+        kun je hier terecht voor unieke{" "}
+        <Link href="/diensten/kaarten-en-uitnodigingen">
+          geboorte-/ en trouwkaarten
+        </Link>
+        .
+      </p>
+    </>
+  );
   return (
     <section className="promotekst container flex items-center justify-center py-8 md:py-32 md:w-1/2">
-      <div
-        className="text-center text-2xl md:text-4xl"
-        dangerouslySetInnerHTML={{ __html: promoTeksthtmlContent }}
-      />
+      <div className="text-center text-2xl md:text-4xl">
+        {promoTeksthtmlContent}
+      </div>
     </section>
   );
 }
